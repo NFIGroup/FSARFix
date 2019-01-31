9755bab7f802a9e61738b71668bd13b9e5bc53bf
New Flyer Industries - ITS <39742714+licenseadminNF@users.noreply.github.com>
2019-01-31 15:00:44 -0600

    Add files via upload

---
+++ FSAR Fix/RightNowConnectService.cs
@@ -0,0 +1,293 @@
+﻿using FSAR_Fix.RightNowService;
+using RightNow.AddIns.AddInViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSAR_Fix
+{
+    class RightNowConnectService
+    {
+        private static RightNowConnectService _rightnowConnectService;
+        private static object _sync = new object();
+        private static RightNowSyncPortClient _rightNowClient;
+
+        private RightNowConnectService()
+        {
+
+        }
+
+        public static RightNowConnectService GetService(IGlobalContext _globalContext)
+        {
+            if (_rightnowConnectService != null)
+            {
+                return _rightnowConnectService;
+            }
+
+            try
+            {
+                lock (_sync)
+                {
+
+                    if (_rightnowConnectService == null)
+                    {
+
+
+                        // Initialize client with current interface soap url 
+                        string url = _globalContext.GetInterfaceServiceUrl(ConnectServiceType.Soap);
+
+                        EndpointAddress endpoint = new EndpointAddress(_globalContext.GetInterfaceServiceUrl(ConnectServiceType.Soap));
+
+                        BasicHttpBinding binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportWithMessageCredential);
+                        binding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.UserName;
+
+                        // Optional depending upon use cases
+                        binding.MaxReceivedMessageSize = 1024 * 1024;
+                        binding.MaxBufferSize = 1024 * 1024;
+                        binding.MessageEncoding = WSMessageEncoding.Mtom;
+
+                        _rightNowClient = new RightNowSyncPortClient(binding, endpoint);
+
+                        BindingElementCollection elements = _rightNowClient.Endpoint.Binding.CreateBindingElements();
+                        elements.Find<SecurityBindingElement>().IncludeTimestamp = false;
+                        _rightNowClient.Endpoint.Binding = new CustomBinding(elements);
+                        WorkspaceAddIn._globalContext.PrepareConnectSession(_rightNowClient.ChannelFactory);
+
+                        _rightnowConnectService = new RightNowConnectService();
+
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                _rightnowConnectService = null;
+
+                //  WorkspaceAddIn.InfoLog(e.Message);
+            }
+
+            return _rightnowConnectService;
+        }
+        /// <summary>
+        /// Return individual fields as per query
+        /// </summary>
+        /// <param name="ApplicationID"></param>
+        /// <param name="Query"></param>
+        /// <returns> array of string delimited by '~'</returns>
+        private string[] GetRNData(string ApplicationID, string Query)
+        {
+            string[] rnData = null;
+            ClientInfoHeader hdr = new ClientInfoHeader() { AppID = ApplicationID };
+
+            byte[] output = null;
+            CSVTableSet data = null;
+
+            try
+            {
+                data = _rightNowClient.QueryCSV(hdr, Query, 50, "~", false, false, out output);
+                string dataRow = String.Empty;
+                if (data != null && data.CSVTables.Length > 0 && data.CSVTables[0].Rows.Length > 0)
+                {
+                    return data.CSVTables[0].Rows;
+                }
+            }
+            catch (Exception ex)
+            {
+                //  WorkspaceAddIn.InfoLog(ex.Message);
+            }
+            return rnData;
+        }
+
+        /// <summary>
+        /// Return  the results as a set of objects
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="objectTemplate"></param>
+        /// <returns></returns>
+        public RNObject[] GetRNDataObject(string query, GenericObject objectTemplate)
+        {
+            RNObject[] rnDataObject = null;
+            QueryResultData[] data = null;
+            ClientInfoHeader clientInfoHeader = new ClientInfoHeader { AppID = "Get RN Object" };
+            try
+            {
+                data = _rightNowClient.QueryObjects(clientInfoHeader, query, new RNObject[] { objectTemplate }, 1000);
+                if (data != null)
+                {
+                    return data[0].RNObjectsResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                //  WorkspaceAddIn.InfoLog("Exception in GetRNDataObject: " + ex.Message);
+            }
+            return rnDataObject;
+        }
+
+
+
+
+
+
+
+        public string[] GetIncFix()
+        {
+            refno = refno.Trim();
+            string query = "SELECT p.ReferenceNumber, p.ID FROM Incident p WHERE  p.CustomFields.CO.ITS > 0 and p.CustomFields.CO.fsar_compliance != 1 AND p.CustomFields.c.incident_type.LookupName = 'Claim' and ID = " ;
+
+            // string query = "SELECT p.ReferenceNumber, p.CustomFields.CO.nf_claim_nmbr FROM Incident p WHERE ID = 3404 ";
+            //  MessageBox.Show(query);
+            string[] resultSet = GetRNData("FSAR wrong", query);
+
+            return resultSet;
+
+
+        }
+
+
+        /// <summary>
+        /// Method which is called to get value of a custom field of Incident record.
+        /// </summary>
+        /// <param name="packageName">The name of the package.</param>
+        /// <param name="fieldName">The name of the custom field.</param>
+        /// <returns>Value of the field</returns>
+        public string GetFieldFromIncidentRecord(IIncident incidentRecord, string packageName, string fieldName)
+        {
+            string value = "";
+            IList<ICustomAttribute> incCustomAttributes = incidentRecord.CustomAttributes;
+
+            foreach (ICustomAttribute val in incCustomAttributes)
+            {
+                if (val.PackageName == packageName)//if package name matches
+                {
+                    if (val.GenericField.Name == packageName + "$" + fieldName)//if field matches
+                    {
+                        if (val.GenericField.DataValue.Value != null)
+                        {
+                            value = val.GenericField.DataValue.Value.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+
+
+
+
+        #region Miscellaneous
+
+
+
+
+        /// <summary>
+        /// Create string type data value
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns> DataValue</returns>
+        private DataValue createStringDataValue(string val)
+        {
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { val };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.StringValue };  //Change this to the type of field
+            return dv;
+        }
+        /// <summary>
+        /// Create string type data value
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns> DataValue</returns>
+        private DataValue createDateDataValue(DateTime val)
+        {
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { val };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.DateValue };  //Change this to the type of field
+            return dv;
+        }
+        /// <summary>
+        /// Create Boolean type data value
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns> DataValue</returns>
+        private DataValue createBooleanDataValue(Boolean val)
+        {
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { val };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.BooleanValue };
+
+            return dv;
+        }
+
+        /// <summary>
+        /// Create integer type data value
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns> DataValue</returns>
+        private DataValue createIntegerDataValue(int val)
+        {
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { val };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.IntegerValue };  //Change this to the type of field
+            return dv;
+        }
+
+        /// <summary>
+        /// Create GenericField object
+        /// </summary>
+        /// <param name="name">Name Of Generic Field</param>
+        /// <param name="dataValue">Vlaue of generic field</param>
+        /// <param name="type">Type of generic field</param>
+        /// <returns> GenericField</returns>
+        private GenericField createGenericField(string name, DataValue dataValue, DataTypeEnum type)
+        {
+            GenericField genericField = new GenericField();
+
+            genericField.dataType = type;
+            genericField.dataTypeSpecified = true;
+            genericField.name = name;
+            genericField.DataValue = dataValue;
+            return genericField;
+        }
+
+        /// <summary>
+        /// Create Named ID type Data Value for NamedID as input
+        /// </summary>
+        /// <param name="namedvalue"></param>
+        /// <returns></returns>
+        private DataValue createNamedID(NamedID namedvalue)
+        {
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { namedvalue };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.NamedIDValue };
+            return dv;
+        }
+
+        /// <summary>
+        /// Create Named ID type data value for integer value as input
+        /// </summary>
+        /// <param name="idVal"></param>
+        /// <returns> DataValue</returns>
+        private DataValue createNamedIDDataValue(long idVal)
+        {
+            ID id = new ID();
+            id.id = idVal;
+            id.idSpecified = true;
+
+            NamedID namedID = new NamedID();
+            namedID.ID = id;
+
+            DataValue dv = new DataValue();
+            dv.Items = new Object[] { namedID };
+            dv.ItemsElementName = new ItemsChoiceType[] { ItemsChoiceType.NamedIDValue };
+
+            return dv;
+        }
+        #endregion
+    }
+}
---
+++ FSAR Fix/WorkspaceAddIn.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.AddIn;
+using System.Drawing;
+using RightNow.AddIns.AddInViews;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Data;
+using System.ComponentModel;
+using System.Linq;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: WorkspaceAddIn.cs
+//
+// Comments:
+//
+// Notes: 
+//
+// Pre-Conditions: 
+//
+////////////////////////////////////////////////////////////////////////////////
+namespace FSAR_Fix
+{
+    public class WorkspaceAddIn : Panel, IWorkspaceComponent2
+    {
+        /// <summary>
+        /// The current workspace record context.
+        /// </summary>
+        private IRecordContext _recordContext;
+        RightNowConnectService _rnConnectService;
+        public static IIncident _incidentRecord;
+        public static IGlobalContext _globalContext { get; private set; }
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="inDesignMode">Flag which indicates if the control is being drawn on the Workspace Designer. (Use this flag to determine if code should perform any logic on the workspace record)</param>
+        /// <param name="RecordContext">The current workspace record context.</param>
+        public WorkspaceAddIn(bool inDesignMode, IRecordContext RecordContext, IGlobalContext GlobalContext)
+        {
+
+            if (!inDesignMode)
+            {
+
+                _recordContext = RecordContext;
+                _globalContext = GlobalContext;
+                //  _recordContext.Saved += _recordContext_Saved;
+                _rnConnectService = RightNowConnectService.GetService(_globalContext);
+
+            }
+        }
+
+        #region IAddInControl Members
+
+        /// <summary>
+        /// Method called by the Add-In framework to retrieve the control.
+        /// </summary>
+        /// <returns>The control, typically 'this'.</returns>
+        public Control GetControl()
+        {
+            return this;
+        }
+
+        #endregion
+
+        #region IWorkspaceComponent2 Members
+
+        /// <summary>
+        /// Sets the ReadOnly property of this control.
+        /// </summary>
+        public bool ReadOnly { get; set; }
+
+        /// <summary>
+        /// Method which is called when any Workspace Rule Action is invoked.
+        /// </summary>
+        /// <param name="ActionName">The name of the Workspace Rule Action that was invoked.</param>
+        public void RuleActionInvoked(string ActionName)
+        {
+        }
+
+        /// <summary>
+        /// Method which is called when any Workspace Rule Condition is invoked.
+        /// </summary>
+        /// <param name="ConditionName">The name of the Workspace Rule Condition that was invoked.</param>
+        /// <returns>The result of the condition.</returns>
+        public string RuleConditionInvoked(string ConditionName)
+        {
+            return string.Empty;
+        }
+
+        public static void SetIncidentField(string pkgName, string fieldName, string value)
+        {
+            if (pkgName == "c")
+            {
+                IList<ICfVal> incCustomFields = _incidentRecord.CustomField;
+                int fieldID = GetCustomFieldID(fieldName);
+                foreach (ICfVal val in incCustomFields)
+                {
+                    if (val.CfId == fieldID)
+                    {
+                        switch (val.DataType)
+                        {
+                            case (int)RightNow.AddIns.Common.DataTypeEnum.BOOLEAN_LIST:
+                            case (int)RightNow.AddIns.Common.DataTypeEnum.BOOLEAN:
+                                if (value == "1" || value.ToLower() == "true")
+                                {
+                                    val.ValInt = 1;
+                                }
+                                else if (value == "0" || value.ToLower() == "false")
+                                {
+                                    val.ValInt = 0;
+                                }
+                                break;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                IList<ICustomAttribute> incCustomAttributes = _incidentRecord.CustomAttributes;
+
+                foreach (ICustomAttribute val in incCustomAttributes)
+                {
+                    if (val.PackageName == pkgName)
+                    {
+                        if (val.GenericField.Name == pkgName + "$" + fieldName)
+                        {
+                            switch (val.GenericField.DataType)
+                            {
+                                case RightNow.AddIns.Common.DataTypeEnum.BOOLEAN:
+                                    if (value == "1" || value.ToLower() == "true")
+                                    {
+                                        val.GenericField.DataValue.Value = true;
+                                    }
+                                    else if (value == "0" || value.ToLower() == "false")
+                                    {
+                                        val.GenericField.DataValue.Value = false;
+                                    }
+                                    break;
+                                case RightNow.AddIns.Common.DataTypeEnum.INTEGER:
+                                    if (value.Trim() == "" || value.Trim() == null)
+                                    {
+                                        val.GenericField.DataValue.Value = null;
+                                    }
+                                    else
+                                    {
+                                        val.GenericField.DataValue.Value = Convert.ToInt32(value);
+                                    }
+                                    break;
+                                case RightNow.AddIns.Common.DataTypeEnum.STRING:
+                                    val.GenericField.DataValue.Value = value;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            return;
+        }
+        public static int GetCustomFieldID(string fieldName)
+        {
+            IList<IOptlistItem> CustomFieldOptList = _globalContext.GetOptlist((int)RightNow.AddIns.Common.OptListID.CustomFields);//92 returns an OptList of custom fields in a hierarchy
+            foreach (IOptlistItem CustomField in CustomFieldOptList)
+            {
+                if (CustomField.Label == fieldName)//Custom Field Name
+                {
+                    return (int)CustomField.ID;//Get Custom Field ID
+                }
+            }
+            return -1;
+        }
+        #endregion
+    }
+
+    [AddIn("Workspace Factory AddIn", Version = "1.0.0.0")]
+    public class WorkspaceAddInFactory : IWorkspaceComponentFactory2
+    {
+        #region IWorkspaceComponentFactory2 Members
+
+            static public IGlobalContext _globalContext; 
+        /// <summary>
+        /// Method which is invoked by the AddIn framework when the control is created.
+        /// </summary>
+        /// <param name="inDesignMode">Flag which indicates if the control is being drawn on the Workspace Designer. (Use this flag to determine if code should perform any logic on the workspace record)</param>
+        /// <param name="RecordContext">The current workspace record context.</param>
+        /// <returns>The control which implements the IWorkspaceComponent2 interface.</returns>
+        public IWorkspaceComponent2 CreateControl(bool inDesignMode, IRecordContext RecordContext)
+        {
+            return new WorkspaceAddIn(inDesignMode, RecordContext,_globalContext);
+    
+        }
+
+        #endregion
+
+        #region IFactoryBase Members
+
+        /// <summary>
+        /// The 16x16 pixel icon to represent the Add-In in the Ribbon of the Workspace Designer.
+        /// </summary>
+        public Image Image16
+        {
+            get { return Properties.Resources.AddIn16; }
+        }
+
+        /// <summary>
+        /// The text to represent the Add-In in the Ribbon of the Workspace Designer.
+        /// </summary>
+        public string Text
+        {
+            get { return "Fix"; }
+        }
+
+        /// <summary>
+        /// The tooltip displayed when hovering over the Add-In in the Ribbon of the Workspace Designer.
+        /// </summary>
+        public string Tooltip
+        {
+            get { return "WorkspaceAddIn Tooltip"; }
+        }
+
+        #endregion
+
+        #region IAddInBase Members
+
+        /// <summary>
+        /// Method which is invoked from the Add-In framework and is used to programmatically control whether to load the Add-In.
+        /// </summary>
+        /// <param name="GlobalContext">The Global Context for the Add-In framework.</param>
+        /// <returns>If true the Add-In to be loaded, if false the Add-In will not be loaded.</returns>
+        public bool Initialize(IGlobalContext GlobalContext)
+        {
+            _globalContext = GlobalContext;
+            return true;
+        }
+
+        #endregion
+    }
+}
